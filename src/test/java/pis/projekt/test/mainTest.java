e15a5a76a4c7af0f3c65d2579d0170b0d45e966d
Drajner <drajner@wp.pl>
2022-11-11 00:21:00 +0100

    added assertJ dependency, changed test to use it, added more stuff gitignore

--- src/test/java/pis/projekt/test/mainTest.java
+++ src/test/java/pis/projekt/test/mainTest.java
@@ -3,12 +3,13 @@ package pis.projekt.test;
 import org.junit.Assert;
 import org.junit.Test;
 import pis.projekt.main;
+import static org.assertj.core.api.Assertions.*;
 
 public class mainTest {
 
     @Test
     public void addTest(){
         int z = main.add(1,2);
-        Assert.assertEquals(z,3);
+        assertThat(z).isEqualTo(3);
     }
 }
