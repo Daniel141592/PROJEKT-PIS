b888af434a65781211622ee09531b5fda30551aa
Drajner <drajner@wp.pl>
2022-12-28 00:23:48 +0100

    Added class for raports and added pdfbox dependencies to pom.xml (1h)

---
+++ src/main/java/pis/projekt/services/Raport.java
@@ -0,0 +1,14 @@
+package pis.projekt.services;
+
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+
+public class Raport {
+    private PDDocument raportDocument;
+    private String reportText;
+    private PDPage raportPage;
+    private PDPageContentStream contentStream;
+
+}
