a7dea476b8244cad70a9ec0764976bb0f921fbbb
Adam Sudoł <jawawu35@gmail.com>
2022-12-28 22:44:17 +0100

    Added unit tests for Pair and Product (with previous commits, 5h)

--- magazyn-backend/src/test/java/pis/projekt/test/productTest.java
+++ magazyn-backend/src/test/java/pis/projekt/test/productTest.java
@@ -8,10 +8,18 @@ import pis.projekt.utils.Pair;
 public class productTest {
 
     @Test
-    public void isSameTest()
+    public void isSameTest1()
     {
         Product prod1 = new Product("beer", 3, 4, 5);
         Product prod2 = new Product("beer", 3, 4, 5);
         Assert.assertTrue(Product.isSame(prod1, prod2));
+    }
+
+    @Test
+    public void isSameTest2()
+    {
+        Product prod1 = new Product("beer", 3, 4, 5);
+        Product prod2 = new Product("nbeer", 3, 4, 5);
+        Assert.assertFalse(Product.isSame(prod1, prod2));
     }
 }
