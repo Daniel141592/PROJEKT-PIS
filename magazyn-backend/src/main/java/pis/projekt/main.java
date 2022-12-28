e4b1334392f6483d90ed6deb16e4d42c62953cd8
Drajner <drajner@wp.pl>
2022-12-28 20:28:37 +0100

    made reports creating work, added creating doc to main for checking purposes, added pdfs to .gitignore(1,5h)

--- magazyn-backend/src/main/java/pis/projekt/main.java
+++ magazyn-backend/src/main/java/pis/projekt/main.java
@@ -1,5 +1,13 @@
 package pis.projekt;
 
+import pis.projekt.services.Magazine;
+import pis.projekt.services.Report;
+import pis.projekt.services.Section;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import java.io.IOException;
+import java.util.Vector;
+
 public class main {
 
     public static int add(int x, int y)
@@ -7,7 +15,22 @@ public class main {
         return x+y;
     }
 
-    public static void main(String[] args){
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("dupa");
+        Vector<Section> sections = new Vector<Section>();
+        Section sect = new Section();
+        for (int i = 0; i < 4; i++) {
+            sections.add(sect);
+        }
+        Magazine mag = new Magazine(7, "benedict", 21, 37, sections);
+        Report rep = new Report(mag);
+        /*PDDocument doc = new PDDocument();
+
+        doc.save("blank.pdf");
+
+        System.out.println("PDF created");
+
+        doc.close();*/
     }
 }
