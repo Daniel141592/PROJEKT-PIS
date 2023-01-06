4e6acbc3bc2bdb6c01f96d9058341e6e11f70fd6
Drajner <drajner@wp.pl>
2023-01-06 23:00:41 +0100

    fixed Chech bug

--- magazyn-backend/src/main/java/pis/projekt/models/IssueHistory.java
+++ magazyn-backend/src/main/java/pis/projekt/models/IssueHistory.java
@@ -8,7 +8,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Entity
-@Table(name = "zlecenia")
+@Table(name = "historia_zlecen")
 public class IssueHistory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
