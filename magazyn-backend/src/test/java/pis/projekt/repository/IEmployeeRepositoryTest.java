b3885e4da3877fe7c28b9db15487bc16a3d87b43
Adam Sudoł <01158935@pw.edu.pl>
2023-01-07 03:40:53 +0100

    Completed IEmployeeRepository tests

--- magazyn-backend/src/test/java/pis/projekt/repository/IEmployeeRepositoryTest.java
+++ magazyn-backend/src/test/java/pis/projekt/repository/IEmployeeRepositoryTest.java
@@ -1,43 +1,76 @@
 package pis.projekt.repository;
 
 import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 import pis.projekt.Config;
 import pis.projekt.models.Employee;
-import pis.projekt.repository.IEmployeeRepository;
 
 @DataJpaTest
 @RunWith(SpringRunner.class)
 @Import({Config.class})
-//@AutoConfigureDataJpa
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 public class IEmployeeRepositoryTest {
-
-//    @Autowired
-//    private TestEntityManager entityManager;
 
     @Autowired
     IEmployeeRepository employeeRepository;
 
+    @Before
+    public void init(){
+        employeeRepository.save(new Employee(1, "milena", "pieta", "mpiet", "anotherslowo", true));
+        employeeRepository.save(new Employee(2, "adam", "sur", "asur", "slowo3", true));
+        employeeRepository.save(new Employee(3, "pawel", "kali", "pkali", "mile", false));
+    }
 
     @Test
     public void isEmptyTest(){
+        employeeRepository.deleteAll();
         Iterable<Employee> employees = employeeRepository.findAll();
         Assertions.assertThat(employees).isEmpty();
     }
 
     @Test
     public void isNotEmptyTest(){
-        employeeRepository.save(new Employee(0, "paweł", "kaliniczenko", "pkal", "chuj", true));
-        Assertions.assertThat(employeeRepository.findEmployeeByLogin("pkal").getLogin()).isEqualTo("pkal");
+        Assertions.assertThat(employeeRepository.findAll().size()).isEqualTo(3);
+    }
+
+    @Test
+    public void findEmployeeByIdTest(){
+        Assertions.assertThat(employeeRepository.findEmployeeById(2).getName()).isEqualTo("adam");
+        Assertions.assertThat(employeeRepository.findEmployeeById(2137)).isNull();
+    }
+
+    @Test
+    public void findEmployeeByLoginTest(){
+        Assertions.assertThat(employeeRepository.findEmployeeByLogin("mpiet").getName()).isEqualTo("milena");
+        Assertions.assertThat(employeeRepository.findEmployeeByLogin("benedicto")).isNull();
+    }
+
+    @Test
+    public void findEmployeeByNameTest(){
+        Assertions.assertThat(employeeRepository.findEmployeeByName("milena").get(0).getLogin()).isEqualTo("mpiet");
+        Assertions.assertThat(employeeRepository.findEmployeeByName("benedicto")).isEmpty();
+    }
+
+    @Test
+    public void findEmployeeByNameSurname(){
+        Assertions.assertThat(employeeRepository.findEmployeeBySurname("kali").get(0).getLogin()).isEqualTo("pkali");
+        Assertions.assertThat(employeeRepository.findEmployeeBySurname("benedicto")).isEmpty();
+    }
+
+    @Test
+    public void findEmployeeByIsManagerTest(){
+        Assertions.assertThat(employeeRepository.findEmployeesByIsManager(true).get(1).getLogin()).isEqualTo("asur");
+        Assertions.assertThat(employeeRepository.findEmployeesByIsManager(false).get(0).getLogin()).isEqualTo("pkali");
     }
 
 }
