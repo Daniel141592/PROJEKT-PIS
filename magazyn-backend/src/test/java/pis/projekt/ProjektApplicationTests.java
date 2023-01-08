879c71a8ce0e81fa2aad1849a2688269259e09a1
Adam Sudoł <01158935@pw.edu.pl>
2023-01-08 04:32:28 +0100

    Corrected pom.xml to handle mvn tests from cmd (2h)

--- magazyn-backend/src/test/java/pis/projekt/ProjektApplicationTests.java
+++ magazyn-backend/src/test/java/pis/projekt/ProjektApplicationTests.java
@@ -1,13 +1,13 @@
 package pis.projekt;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ProjektApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
