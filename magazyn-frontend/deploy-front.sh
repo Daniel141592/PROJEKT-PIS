0c6266f4fdec3e10f25add1100c6cac0e409191d
Daniel Kobiałka <daniel.kobialka.stud@pw.edu.pl>
2023-01-09 17:21:03 +0100

    Frontend deploy script updated

--- magazyn-frontend/deploy-front.sh
+++ magazyn-frontend/deploy-front.sh
@@ -1,6 +1,15 @@
 git clone git@github.com:Daniel141592/PROJEKT-PIS.git
 cd PROJEKT-PIS/magazyn-frontend
+
 sudo apt install -y npm
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm install 19.3.0
+
 npm i
 npm run dev  # TODO poprawić na produkcyjną instalację
 
